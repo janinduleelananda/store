1c2863e92273cdcd0ce8e70e5f592f14ff099149
janinduleelananda <janindur@gmail.com>
2020-06-09 16:11:38 +0530

    added search functionality

--- store/settings.py
+++ store/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'search_app.apps.SearchAppConfig',
     'shop.apps.ShopConfig',
     'django.contrib.admin',
     'django.contrib.auth',
