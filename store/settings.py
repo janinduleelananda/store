0142b7cdd40e77ccee1fa960c5368cb0206e6f29
janinduleelananda <janindur@gmail.com>
2020-06-16 00:32:19 +0530

    added local support

--- store/settings.py
+++ store/settings.py
@@ -149,3 +149,8 @@ EMAIL_PORT= '587'
 EMAIL_USE_TLS= True
 EMAIL_HOST_USER='janindur@gmail.com'
 EMAIL_HOST_PASSWORD='kitty janindu'
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
